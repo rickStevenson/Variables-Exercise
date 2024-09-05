1a7f9bebdce82022806a2e5ed34014759d8dc2e9
Rick Stevenson <dadsathome1@gmail.com>
2024-09-05 12:39:31 -0600

    Add .gitignore to exclude .vs folder

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Rick";
+            int age = 52;
+            char middleInitial = 'R';
+            bool isOver18 = true;
+            double currentTemp = 87.5;
+            decimal bankAccount = 200150.50m;
+
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is Over 18: {isOver18}");
+            Console.WriteLine($"Current Body Temp: {currentTemp}");
+            Console.WriteLine($"BankAccount: {bankAccount}");
         }
     }
 }
